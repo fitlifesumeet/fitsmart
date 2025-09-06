b3d0ff6081fde2f06e76a2687654e40210307215
fitlifesumeet <fitlifesumeet@gmail.com>
2025-09-06 14:52:04 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return (
 
-    <div className="min-h-screen bg-cover bg-center" style={{ backgroundImage: "url('/images/bg-photo.jpg')" }}>
+    <div className="min-h-screen bg-cover bg-center" style={{ backgroundImage: "url('/images/bg-photo.jpeg')" }}>
   <div className="min-h-screen bg-black/40 p-4 md:p-8">
     <Component {...pageProps} />
   </div>
